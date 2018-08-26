d8f4c749db64e6ac04d48d135b93d5c6cdf22a7d
JinYao <kimmyoo@gmail.com>
2018-08-26 18:22:48 -0400

    some change

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,8 +1,8 @@
 class Solution ():
     def insertion_sort(self, input_list):
         """input type: list"""
-        count = len(input_list)
-        for n in range(count):
+        count = len(input_list) # no need to start from 0.
+        for n in range(1, count):
             cur = input_list[n]
             j = n
             while j > 0 and input_list [j-1] > cur:
@@ -11,6 +11,6 @@ class Solution ():
             input_list[j] = cur
 
 s = Solution()
-test_list = [9, 8, 7, 6, 5, 4]
+test_list = [8, 7, 6, 5, 4]
 s.insertion_sort(test_list)
 print(test_list)
