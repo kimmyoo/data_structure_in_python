68f3ef87ba84aca91916e732f4d5437c621633ec
JinYao <kimmyoo@gmail.com>
2018-09-15 20:48:16 -0400

    quick_sort.py added

---
+++ sorting/quick_sort.py
@@ -0,0 +1,34 @@
+def quick_sort(input_list):
+    if len(input_list) < 2:
+        return
+    
+    pivot = input_list[0]
+    lt_list = []
+    gt_list = []
+    et_list = []
+    
+    #divide 
+    while len(input_list) != 0:
+        if input_list[0] > pivot:
+            gt_list.append(input_list.pop(0))
+        if input_list[0] < pivot:
+            lt_list.append(input_list.pop(0))
+        else:
+            et_list.append(input_list.pop(0))
+    #conquer
+    quick_sort(lt_list)
+    quick_sort(gt_list)
+    
+    #merge back
+    while len(lt_list) != 0:
+        input_list.append(lt_list.pop(0))
+    while len(et_list) != 0:
+        input_list.append(et_list.pop(0))
+    while len(gt_list) != 0:
+        input_list.append(gt_list.pop(0))
+
+
+
+test_list = [13, 6, 0, 1, -1, -9]
+quick_sort(test_list)
+print (test_list)
