1aff0eb80ac2ba788973bee51a3fb264722c3b04
JinYao <kimmyoo@gmail.com>
2019-01-05 22:51:14 -0500

    insertion sort

--- sorting/insertion_sort/insertion_sort.py
+++ sorting/insertion_sort/insertion_sort.py
@@ -1,13 +1,16 @@
 class Solution ():
     def insertion_sort(self, input_list):
         """input type: list"""
-        count = len(input_list) # no need to start from 0.
+        count = len(input_list)
+        # no need to start from 0.
         for n in range(1, count):
             cur = input_list[n]
             j = n
             while j > 0 and input_list [j-1] > cur:
                 input_list[j] = input_list [j-1]
                 j-=1
+            #put cur to where it is supposed to be
+            #if no wapping happens over here, the element is not swapped
             input_list[j] = cur
 
 s = Solution()
